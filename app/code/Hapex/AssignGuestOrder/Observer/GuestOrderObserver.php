1daa0f3b7a628f21b041f32a63d3c3d3c88fac36
Oleg K <voknevark@gmail.com>
2020-08-28 16:18:11 -0700

    Update Observer\GuestOrderObserver.php

--- app/code/Hapex/AssignGuestOrder/Observer/GuestOrderObserver.php
+++ app/code/Hapex/AssignGuestOrder/Observer/GuestOrderObserver.php
@@ -30,20 +30,40 @@ class GuestOrderObserver extends BaseObserver
             $this->helperData->log("Starting Guest Order Observer");
             $order = $observer->getEvent()->getOrder();
             $this->helperData->log("Checking for Guest Order");
-            if ($this->helper->isModuleEnabled() && $this->helperOrder->isGuestOrder($order)) {
-                $orderNumber = $order->getIncrementId();
-                $this->helperData->log("Guest Order #$orderNumber detected");
-                $this->helperData->log("Attempting to assign to existing customer");
-                if ($this->helperOrder->assignGuestOrder($order)) {
-                    $this->helperData->log("Guest Order assigned successfully");
-                } else {
-                    $this->helperData->log("Could not assign guest order to existing customer");
-                }
+            if ($this->helperData->isModuleEnabled()) {
+                $this->processOrder($order);
             }
             $this->helperData->log("Ending Guest Order Observer");
         } catch (\Exception $e) {
             $this->helperLog->errorLog(__METHOD__, $e->getMessage());
             $this->messageManager->addErrorMessage($e->getMessage());
+        }
+    }
+
+    private function processOrder($order = null)
+    {
+        try {
+            switch ($this->helperOrder->isGuestOrder($order)) {
+                case true:
+                    $orderNumber = $order->getIncrementId();
+                    $this->helperData->log("Guest Order #$orderNumber detected");
+                    $this->helperData->log("Attempting to assign to existing customer");
+                    switch ($this->helperOrder->assignGuestOrder($order)) {
+                        case true:
+                            $this->helperData->log("Guest Order assigned successfully");
+                            break;
+
+                        default:
+                            $this->helperData->log("Could not assign guest order to existing customer");
+                            break;
+                    }
+                    break;
+
+                default:
+                    $this->helperData->log("No Guest Order detected");
+                    break;
+            }
+        } catch (\Exception $e) {
         }
     }
 }
